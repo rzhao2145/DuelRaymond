89ce28b8bf3149f50bc3c56babdc20418322edf5
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2017-09-25 15:01:00 -0400

    9/25/17

--- DuelRaymond/src/duel/CharacterA.java
+++ DuelRaymond/src/duel/CharacterA.java
@@ -3,13 +3,14 @@ package duel;
 public class CharacterA implements Dueler {
 	
 	private String name;
-	private int hp;
+	private int hp2;
+	private int rounds2 = 0;
 	
 	public CharacterA(){
 		
 	}
 	public void taunt() {
-		String taunt =  ("Lul");
+		String taunt =  ("You can't beat me");
 		System.out.println(taunt);
 	}
 	
@@ -19,17 +20,34 @@ public class CharacterA implements Dueler {
 	}
 	
 	public void setStartingHP(int hp) {
+		hp2 = hp;
 		System.out.println(hp);
 	}
 	
 	public int getHP() {
 		
-		return hp;
+		return hp2;
 	}
 	
 	public boolean determineIfOpponentIsFair(Dueler d, int target) {
+		if(d.getHP() == target) {
+			return true;
+		} else {
+			return false;
+		}
 		
-		return true;
+	}
+	public int getAction(Object caller) {
+		if(rounds2 == 0) {
+			rounds2++;
+			return 0;
+		} else {
+			rounds2++;
+			return (int)(Math.random()*3);
+		}
+	}
+	public void hit(Object caller) {
+		hp2 = hp2 - 10;
 	}
 	
 }
--- DuelRaymond/src/duel/CharacterB.java
+++ DuelRaymond/src/duel/CharacterB.java
@@ -3,7 +3,9 @@ package duel;
 public class CharacterB implements Dueler {
 	
 	private String name;
-	private int hp;
+	private int hp2;
+	private int rounds2 = 0;
+
 	
 	public CharacterB(){
 		
@@ -19,17 +21,32 @@ public class CharacterB implements Dueler {
 	}
 	
 	public void setStartingHP(int hp) {
+		hp2 = hp;
 		System.out.println(hp);
 	}
 	
 	public int getHP() {
 		
-		return hp;
+		return hp2;
 	}
 	
 	public boolean determineIfOpponentIsFair(Dueler d, int target) {
-		
-		return true;
+		if(d.getHP() == target) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-	
+	public int getAction(Object caller) {
+		if(rounds2 == 0) {
+			rounds2++;
+			return 0;
+		} else {
+			rounds2++;
+			return (int)(Math.random()*3);
+		}
+	}
+	public void hit(Object caller) {
+		hp2 = hp2 - 10;
+	}
 }
